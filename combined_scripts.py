e50f1d63161dbd9f0277812da9c6defc9c5ac334
LordGrimmauld <soeren@benjos.de>
2019-12-07 13:20:47 +0100

    slight improvements in readability

--- combined_scripts.py
+++ combined_scripts.py
@@ -14,25 +14,26 @@ import win32api
 import threading
 
 
-def getForegroundWindowTitle() -> Optional[str]:
-    hWnd = windll.user32.GetForegroundWindow()
-    length = windll.user32.GetWindowTextLengthW(hWnd)
+def get_foreground_window_title() -> Optional[str]:
+    hwnd = windll.user32.GetForegroundWindow()
+    length = windll.user32.GetWindowTextLengthW(hwnd)
     buf = create_unicode_buffer(length + 1)
-    windll.user32.GetWindowTextW(hWnd, buf, length + 1)
+    windll.user32.GetWindowTextW(hwnd, buf, length + 1)
 
     return buf.value if buf.value else None
 
 
 try:
     with open("highscores.json", "r") as f:
-        highscores = json.load(f)
+        high_scores = json.load(f)
 except FileNotFoundError as e:
     with open("highscores.json", "w") as f:
         json.dump({}, f)
-        highscores = {}
+        high_scores = {}
 
-click_delay = .005  # delay of autoclick-functionality
-blockkeys = "wasdepuml1234567"  # keys that might block the combination of l + alt + prtscn
+click_delay = .005  # delay of auto-click-functionality
+block_other_click_spam = True  # blocks the start of right click-spam if left click is already active and vice versa
+block_keys = "wasdepuml1234567"  # keys that might block the combination of l + alt + print_screen
 lock = True  # start of the lock (depending on your application, change this to True)
 blacklist = {"diep", "youtube", "minecraft"}  # everything you want to block, add your own games here.
 explicit_whitelist = {"pycharm",
@@ -43,21 +44,21 @@ def error_handler(method):
     def tested(*args, **kw):
         try:
             return method(*args, **kw)
-        except Exception as e:
-            print(e)
-
+        except Exception as exception:
+            print(exception)
     return tested
 
 
+@error_handler
 def take_pic():
-    if "diep.io" in getForegroundWindowTitle():
-        [keyboard.block_key(k) for k in blockkeys]
+    if "diep.io" in get_foreground_window_title():
+        [keyboard.block_key(k) for k in block_keys]
         keyboard.press("l")
         time.sleep(.1)
         keyboard.press_and_release("alt+prtscn")
         time.sleep(.05)
         keyboard.release("l")
-        [keyboard.unblock_key(k) for k in blockkeys]
+        [keyboard.unblock_key(k) for k in block_keys]
         time.sleep(.1)
         img = ImageGrab.grabclipboard()
         time.sleep(.1)
@@ -65,8 +66,8 @@ def take_pic():
         for x in range(img2.width):
             for y in range(img2.height):
                 p = img2.getpixel((x, y))
-                img2.putpixel((x, y), (0, 0, 0) if all(map(lambda x: x > 185, p)) and not all(
-                    map(lambda x: 207 > x > 195, p)) and p not in [(198, 198, 198), (205, 205, 205),
+                img2.putpixel((x, y), (0, 0, 0) if all(map(lambda c: c > 185, p)) and not all(
+                    map(lambda c: 207 > c > 195, p)) and p not in [(198, 198, 198), (205, 205, 205),
                                                                    (202, 202, 202)] else (0xff, 0xff, 0xff))
         img2.save("testimg.png", "PNG")
         time.sleep(.1)
@@ -81,48 +82,44 @@ def take_pic():
                 _class = l.strip().split()[2].decode()
         if _score is not None and _class is not None:
             print(_class, "with", _score)
-            if _class in highscores:
-                if _score > highscores[_class][0]:
-                    if highscores[_class][1] in os.listdir("highscores"):
-                        os.remove("highscores\\" + highscores[_class][1])
+            if _class in high_scores:
+                if _score > high_scores[_class][0]:
+                    if high_scores[_class][1] in os.listdir("highscores"):
+                        os.remove("highscores\\" + high_scores[_class][1])
                 else:
                     return
             filename = _class + "-" + str(_score) + "-" + datetime.now().strftime("%m~%d~%Y-%H~%M") + ".png"
-            highscores[_class] = (_score, filename)
+            high_scores[_class] = (_score, filename)
             img.save("highscores\\" + filename, "PNG")
-            with open("highscores.json", "w") as f:
-                json.dump(highscores, f)
+            with open("highscores.json", "w") as high_scores_json_file:
+                json.dump(high_scores, high_scores_json_file)
         else:
             print(text)
             take_pic()  # Fixme: maybe not a good idea...
         # Imgur??? https://steemit.com/programming/@synapse/how-to-upload-images-to-imgur-using-python
 
 
-survival = False
-
-
 def toggle_survival():
-    global survival
-    if "diep.io" in getForegroundWindowTitle():
-        survival = not survival
-        print("survival mode", "activated" if survival else "deactivated")
+    global survival_activated
+    if "diep.io" in get_foreground_window_title():
+        survival_activated = not survival_activated
+        print("survival mode", "activated" if survival_activated else "deactivated")
 
 
 def minimize_blacklisted():
     if not lock:  # check the lock
         return
-    hWnd = windll.user32.GetForegroundWindow()  # get foreground window
-    length = windll.user32.GetWindowTextLengthW(hWnd)
+    hwnd = windll.user32.GetForegroundWindow()  # get foreground window
+    length = windll.user32.GetWindowTextLengthW(hwnd)
     buf = create_unicode_buffer(length + 1)
-    windll.user32.GetWindowTextW(hWnd, buf, length + 1)  # read the title
-    # print(buf.value)
+    windll.user32.GetWindowTextW(hwnd, buf, length + 1)  # read the title
     if buf.value is not None and any(map(lambda x: x in buf.value.lower(), blacklist)) and not any(
             map(lambda x: x in buf.value.lower(),
                 explicit_whitelist)):  # is any of the blacklisted items contained in the active windows title?
-        win32gui.ShowWindow(hWnd, win32con.SW_MINIMIZE)
+        win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
 
 
-def toggle_game_close():  # toggle_game_close the lock and give a message about the new status in the console.
+def toggle_game_close():  # toggle the lock and give a message about the new status in the console.
     global lock
     lock = not lock
     print("Lock is now " + ("active" if lock else "inactive"))
@@ -137,7 +134,7 @@ def left_click(x=None, y=None):
     if x is not None and y is not None:
         win32api.SetCursorPos((x, y))
     else:
-        flags, hcursor, (x, y) = win32gui.GetCursorInfo()
+        flags, cursor, (x, y) = win32gui.GetCursorInfo()
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
@@ -146,7 +143,7 @@ def right_click(x=None, y=None):
     if x is not None and y is not None:
         win32api.SetCursorPos((x, y))
     else:
-        flags, hcursor, (x, y) = win32gui.GetCursorInfo()
+        flags, cursor, (x, y) = win32gui.GetCursorInfo()
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
 
@@ -155,7 +152,7 @@ def right_click(x=None, y=None):
 def survive():
     n = 0
     while True:
-        if survival and "diep.io" in getForegroundWindowTitle():
+        if survival_activated and "diep.io" in get_foreground_window_title():
             keyboard.press("a")
             time.sleep(2)
             keyboard.release("a")
@@ -170,13 +167,13 @@ def survive():
 
 
 @error_handler
-def autoclick_left():
+def auto_click_left():
     global left_click_lock_active, right_click_lock_active
     while True:
         while (not (win32api.GetKeyState(0x01) not in (1, 0, -1) and keyboard.is_pressed(
                 'ctrl+shift'))) or right_click_lock_active:
             time.sleep(.03)
-        left_click_lock_active = True
+        left_click_lock_active = block_other_click_spam
         while keyboard.is_pressed('ctrl+shift'):
             left_click()
             time.sleep(click_delay)
@@ -184,13 +181,13 @@ def autoclick_left():
 
 
 @error_handler
-def autoclick_right():
+def auto_click_right():
     global right_click_lock_active, left_click_lock_active
     while True:
         while (not (win32api.GetKeyState(0x02) not in (1, 0, -1) and keyboard.is_pressed(
                 'ctrl+shift'))) or left_click_lock_active:
             time.sleep(.03)
-        right_click_lock_active = True
+        right_click_lock_active = block_other_click_spam
         while keyboard.is_pressed('ctrl+shift'):
             right_click()
             time.sleep(click_delay)
@@ -199,6 +196,7 @@ def autoclick_right():
 
 right_click_lock_active = False
 left_click_lock_active = False
+survival_activated = False
 blacklist = {i.lower() for i in blacklist}
 explicit_whitelist = {i.lower() for i in explicit_whitelist}
 
@@ -210,10 +208,14 @@ if __name__ == "__main__":
     keyboard.add_hotkey("p", take_pic)
     keyboard.add_hotkey("v", toggle_survival)
 
-    threads = []
-    threads.append(threading.Thread(target=autoclick_left))
-    threads.append(threading.Thread(target=autoclick_right))
+    # permanent infinite loops
+    threads = list()
+    threads.append(threading.Thread(target=auto_click_left))
+    threads.append(threading.Thread(target=auto_click_right))
     threads.append(threading.Thread(target=survive))
     for thread in threads:
         thread.start()
-    keyboard.wait()
+
+    # just run forever
+    while True:
+        time.sleep(1)
