418030b3b6643ca2ed65a6f97c7ac52b32bb02ce
LordGrimmauld <soeren@benjos.de>
2019-12-08 22:39:05 +0100

    black screen overlay has no more icon in task bar

--- combined_scripts.py
+++ combined_scripts.py
@@ -1,4 +1,6 @@
 from PIL import ImageGrab
+import tkinter
+import pywintypes
 import time
 from typing import Optional
 from ctypes import windll, create_unicode_buffer
@@ -12,7 +14,6 @@ import win32con  # part of the pywin32 lib bundle (install with pip install pywi
 import win32gui
 import win32api
 import threading
-
 
 try:
     with open("highscores.json", "r") as f:
@@ -194,28 +195,60 @@ def auto_click_right():
         right_click_lock_active = False
 
 
+def toggle_black_screen(widget):
+    global globalExStyle, label, old_hwnd, new_hwnd, cycle
+    cycle += 1
+    if cycle % 3 == 2:
+        return
+    globalExStyle ^= win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE
+    switch = globalExStyle & (win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE)
+    win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, globalExStyle)
+    widget.master.wm_attributes("-transparentcolor", "black" if switch else "green")
+    if not switch:
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 10, 10, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 10, 10, 0, 0)
+
+
+
+
 right_click_lock_active = False
 left_click_lock_active = False
 survival_activated = False
 blacklist = {i.lower() for i in blacklist}
 explicit_whitelist = {i.lower() for i in explicit_whitelist}
+cycle = 0
+
 
 if __name__ == "__main__":
+    label = tkinter.Label(text=' ', font=('Times New Roman', '12'), fg='black', bg="black")
+    label.config(width=360, height=120, cursor="none")
+    label.master.overrideredirect(True)
+    label.master.geometry("+0+0")
+    label.master.lift()
+    label.master.wm_attributes("-topmost", True)
+    label.master.wm_attributes("-transparentcolor", "black")
+    label.bind("<Button-1>", lambda event: toggle_black_screen(event.widget))
+    hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
+    globalExStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOOLWINDOW
+    win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, globalExStyle)
+    label.pack()
+
     # Keybindings
     keyboard.add_hotkey('ctrl+ü', toggle_game_close)
     keyboard.add_hotkey('ctrl+shift+ü', exit_script)
     keyboard.add_hotkey('ctrl+´', minimize_blacklisted)
     keyboard.add_hotkey("p", take_pic)
     keyboard.add_hotkey("v", toggle_survival)
+    keyboard.add_hotkey("ctrl+shift+e", callback=toggle_black_screen, args=(label,))
 
     # permanent infinite loops
     threads = list()
     threads.append(threading.Thread(target=auto_click_left))
     threads.append(threading.Thread(target=auto_click_right))
     threads.append(threading.Thread(target=survive))
+
     for thread in threads:
         thread.start()
 
     # just run forever
-    while True:
-        time.sleep(1)
+    label.mainloop()
--- mute_screen.py
+++ mute_screen.py
@@ -3,42 +3,24 @@ import win32api
 import win32con
 import pywintypes
 import keyboard
-from ctypes import wintypes
-import ctypes
-user32 = ctypes.WinDLL("user32")
-
-
-user32.FindWindowW.restype = wintypes.HWND
-user32.FindWindowW.argtypes = (
-    wintypes.LPCWSTR, # lpClassName
-    wintypes.LPCWSTR) # lpWindowName
-
-user32.ShowWindow.argtypes = (
-    wintypes.HWND, # hWnd
-    ctypes.c_int)  # nCmdShow
-
-
-def hide_taskbar():
-    hWnd = user32.FindWindowW(u"Shell_traywnd", None)
-    user32.ShowWindow(hWnd, 0)
-
-def unhide_taskbar():
-    hWnd = user32.FindWindowW(u"Shell_traywnd", None)
-    user32.ShowWindow(hWnd, 5)
 
 
 def toggle_black_screen(widget):
-    global globalExStyle, label, old_hwnd, new_hwnd
+    global globalExStyle, label, old_hwnd, new_hwnd, cycle
+    cycle += 1
+    if cycle % 3 == 2:
+        return
     globalExStyle ^= win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE
     switch = globalExStyle & (win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE)
     win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, globalExStyle)
     widget.master.wm_attributes("-transparentcolor", "black" if switch else "green")
-    if switch:
-        unhide_taskbar()
-    else:
-        hide_taskbar()
+    if not switch:
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 10, 10, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 10, 10, 0, 0)
 
 
+
+cycle = 0
 label = tkinter.Label(text=' ', font=('Times New Roman', '12'), fg='black', bg="black")
 label.config(width=360, height=120, cursor="none")
 label.master.overrideredirect(True)
@@ -48,7 +30,7 @@ label.master.wm_attributes("-topmost", True)
 label.master.wm_attributes("-transparentcolor", "black")
 label.bind("<Button-1>", lambda event: toggle_black_screen(event.widget))
 hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
-globalExStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE
+globalExStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOOLWINDOW
 win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, globalExStyle)
 label.pack()
 keyboard.add_hotkey("ctrl+shift+e", callback=toggle_black_screen, args=(label,))
